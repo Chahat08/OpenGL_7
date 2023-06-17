a6120c057130522345d3f14f8a7cca15d6e5717f
Chahat Kalsi <chahat.ck88@gmail.com>
2023-06-17 19:37:35 +0530

    output the vertex position to be used in fragment shader

--- OpenGL_7/shaders/fragmentShader.frag
+++ OpenGL_7/shaders/fragmentShader.frag
@@ -1,6 +1,6 @@
 #version 330 core
+in vec4 vertexPosition;
 out vec4 FragColor;
-uniform vec4 greenColor;
 void main(){
-	FragColor = greenColor;
+	FragColor = vertexPosition;
 }
--- OpenGL_7/source.cpp
+++ OpenGL_7/source.cpp
@@ -74,12 +74,8 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		float timeValue = glfwGetTime();
-		float greenValue = sin(timeValue) / 2 + 0.5;
-		int vertexColorLocation = glGetUniformLocation(shader.getID(), "greenColor");
 		
 		shader.use();
-		glUniform4f(vertexColorLocation, 0.0, greenValue, 0.0, 0.0);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
